4c5eb55065453fc9bebf02fb140ba80a9d19c40d
Lucille <82943831+luciadobre@users.noreply.github.com>
2023-10-27 11:43:24 +0300

    random test

--- main_test.go
+++ main_test.go
@@ -3,21 +3,27 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
 	"testing"
 )
 
-func TestGETRequest(t *testing.T) {
-	t.Run("returns request", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
+func TestHandleTodoRequest(t *testing.T) {
+	t.Run("get response", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "/todos/1", nil)
+		rr := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		target, _ := url.Parse("https://jsonplaceholder.typicode.com")
 
-		got := response.Body.String()
-		want := "20"
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			handleTodoRequest(w, req, target, httputil.NewSingleHostReverseProxy(target))
+		}).ServeHTTP(rr, req)
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+		expected := `{"userId":1,"id":1,"title":"delectus aut autem","completed":false,"foo":"bar"}`
+		got := rr.Body.String()
+
+		if rr.Code != http.StatusOK || got != expected {
+			t.Errorf("handler returned unexpected result: got %s, want %s", got, expected)
 		}
 	})
 }

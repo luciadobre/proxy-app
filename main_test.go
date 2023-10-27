98d4140a14699c1ae05da7d2547db8ac7f0bfbee
Lucille <82943831+luciadobre@users.noreply.github.com>
2023-10-27 11:48:32 +0300

    test update

--- main_test.go
+++ main_test.go
@@ -10,20 +10,20 @@ import (
 
 func TestHandleTodoRequest(t *testing.T) {
 	t.Run("get response", func(t *testing.T) {
-		req, _ := http.NewRequest("GET", "/todos/1", nil)
-		rr := httptest.NewRecorder()
+		request, _ := http.NewRequest("GET", "/todos/1", nil)
+		reply := httptest.NewRecorder()
 
 		target, _ := url.Parse("https://jsonplaceholder.typicode.com")
-
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			handleTodoRequest(w, req, target, httputil.NewSingleHostReverseProxy(target))
-		}).ServeHTTP(rr, req)
+		pretendServer := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			handleTodoRequest(w, request, target, httputil.NewSingleHostReverseProxy(target))
+		})
+		pretendServer.ServeHTTP(reply, request)
 
 		expected := `{"userId":1,"id":1,"title":"delectus aut autem","completed":false,"foo":"bar"}`
-		got := rr.Body.String()
+		got := reply.Body.String()
 
-		if rr.Code != http.StatusOK || got != expected {
-			t.Errorf("handler returned unexpected result: got %s, want %s", got, expected)
+		if got != expected {
+			t.Errorf("Got %s, want %s", got, expected)
 		}
 	})
 }
